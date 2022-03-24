f9f1075db46d6546951d97a36dd397b99117fb70
etherio <a>
2022-03-24 22:10:23 +0630

    fixed erro after adding firebaseconfiguration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,5 +4,6 @@ import { fadeInLeft } from './core/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  animations: [fadeInLeft],
 })
-export class AppComponent { }
+export class AppComponent {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { IconsModule } from './icons/icons.module';
 import { initializeFirebase } from './firebase/firebase.init';
-import { FirebaseService } from './firebase.service';
+import { FirebaseService } from './firebase/firebase.service';
 
 @NgModule({
   declarations: [],
-  imports: [
-    IconsModule,
-  ],
+  imports: [IconsModule],
   providers: [
     {
       provide: APP_INITIALIZER,
